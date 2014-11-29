8fd857a13de0ece25a06140249e69e2bf965c32f
Gonzalo Mendez <zalomea@gmail.com>
2014-11-29 17:44:37 +0100

    Adding Translator.js and examples

--- README.md
+++ README.md
@@ -1,4 +1,65 @@
-translator-js
+Translator.js
 =============
 
-JavaScript library for HTML internationalization and localization.
+**A JavaScript library for HTML internationalization and localization.**
+
+Internationalize your HTML App with JavaScript easily. Perfect for your 
+PhoneGap App or Angular.js App.
+
+## How to use it
+
+Translator.js is a native JS plugin that don't need any special plugin, you 
+only have to include the library and add the tags and attributes.
+
+- Translation tags
+
+Use the tag `<translatag>` and the attribute `data-translate` with the index 
+from your dictionary.
+```
+<button><translatag data-translate="Word"></translatag></button>
+```
+
+If you active the HTML friendly you must write
+```
+<button data-translate="Word"></button>
+```
+
+- Code example to add plugin to your HTML document:
+
+```
+Translator({
+	language : 'en',
+	dictionary : dictionary,
+	autostart : true,
+});
+```
+- Sample of dictionary variable:
+
+```
+var dictionary = {
+	'en' : {
+		'Jugar' : 'Play',
+		'Traducir a' : 'Translate to',
+	},
+	'es' : {
+		'Jugar' : 'Jugar',
+		'Traducir a' : 'Traducir a',
+	},
+}
+```
+
+## Options
+
+The supported options are:
+
+*`language` : Set the active language.
+
+*`dictionary` : Set the dictionary variable.
+
+*`autostart` : Run translation on document load (`false` by default).
+
+*`htmlfriendly` : Make your code HTML friendly, if true don't use the tag `translatag` (`false` by default).
+
+## Examples
+
+See the examples folder for an example of the usage.
---
+++ js/translator.js
@@ -0,0 +1,77 @@
+var Translator = function(options){
+	var t = {
+		lang : null,
+		dict : [],
+		htmlfriend : false,
+	};
+	if(typeof options == 'undefined'){
+		options = {};
+	}
+
+	if(typeof options.language != 'undefined'){
+		t.lang = options.language;
+	}
+	if(typeof options.dictionary != 'undefined'){
+		t.dict = options.dictionary;
+	}
+	if(typeof options.htmlfriendly != 'undefined' && options.htmlfriendly){
+		t.htmlfriend = true;
+	}
+
+	var translate = function(index){
+		if(t.lang == null){
+			return index;
+		}else if(t.dict[t.lang] != null){
+			if(t.dict[t.lang][index] != null){
+				return t.dict[t.lang][index];
+			}
+		}
+		return index;
+	};
+
+	t.setLanguage = function(language){
+		t.lang = language;
+	};
+	t.setLanguageRun = function(language){
+		t.setLanguage(language);
+		t.runTranslation();
+	};
+
+	t.addTranslation = function(language, index, translation){
+		if(typeof translation == 'undefined'){
+			t.dict[language] = index;
+		}else{
+			if(typeof t.dict[language] == 'undefined'){
+				t.dict[language] = [];
+			}
+			t.dict[language][index] = translation;
+		}
+	};
+
+	t.runTranslation = function() {
+		var elements = [];
+		if(t.htmlfriend){
+			elements = document.getElementsByTagName('*');
+			for (var i = 0; i < elements.length; i++) {
+				if(elements.item(i).getAttribute('data-translate') != null){
+					elements.item(i).innerHTML = translate(elements.item(i).getAttribute('data-translate'));
+				}
+			}
+		}else{
+			elements = document.getElementsByTagName('translatag');
+			for (var i = 0; i < elements.length; i++) {
+				elements.item(i).innerHTML = translate(elements.item(i).getAttribute('data-translate'));
+			}
+		}
+
+	};
+
+	if(typeof options.autostart != 'undefined' && options.autostart){
+		if(window.addEventListener)
+			window.addEventListener('load', t.runTranslation, false);
+		else if(element.attachEvent)
+			element.attachEvent('onload', t.runTranslation);
+	}
+
+	return t;
+};
